ad272dd70976ac9ad121860843927575b3dcab14
Samuel Curtin <stuckercurtin@gmail.com>
2023-03-24 16:15:32 -0400

    Func created to randomly select an item from list, the first being destinations

--- 1starter_lists.py
+++ 1starter_lists.py
@@ -2,3 +2,14 @@ destinations = ["Mexico", "New York", "Buffalo", "Cleveland"]
 restaurants = ["Steak House", "Gastropub", "Diner", "Deli"] 
 transportations = ["Plane", "Train", "Automobile"] 
 entertainments = ["See a show", "Go to a Museam", "Explore the City", "Relax"]
+
+import random
+
+def display_list(list):
+    for item in list:
+        random_item = random.choice(list)
+        print(random_item) 
+        return display_list
+
+
+display_list(destinations)

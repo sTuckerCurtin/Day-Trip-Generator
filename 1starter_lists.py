753cce22dfedf79c439977d4f28a473608101dc8
Samuel Curtin <stuckercurtin@gmail.com>
2023-03-24 16:44:16 -0400

    Here I have built out the bones for the full trip generator

--- 1starter_lists.py
+++ 1starter_lists.py
@@ -11,6 +11,21 @@ def display_list(list):
         print(random_item)
         return display_list
 
-display_list(destinations)
-display_list(restaurants)
-display_list(entertainments)
+#display_list(destinations)
+#display_list(restaurants)
+#display_list(entertainments)
+#display_list(transportations)
+
+
+
+def trip_generator(list1, list2, list3):
+    for trips in list1:
+        random_trip1 = random.choice(list1)
+    for trips in list2:
+        random_trip2 = random.choice(list2)
+    for trips in list3:
+            random_trip3 = random.choice(list3)
+    print(random_trip1, random_trip2, random_trip3)
+    return trip_generator
+
+trip_generator(destinations, transportations, entertainments)

28069b958351322ef4f36c3a4b15aa07602fa7db
Samuel Curtin <stuckercurtin@gmail.com>
2023-03-27 16:34:50 -0400

    This last and fian commit full expresses all of the directions of the project but unlike the last one there are more single function responsibilities

--- 1starter_lists.py
+++ 1starter_lists.py
@@ -11,55 +11,67 @@ def display_list(list):
          print(random_item)
          return display_list
 
-display_list(destinations)
-display_list(restaurants)
-display_list(entertainments)
-display_list(transportations)
+# display_list(destinations)
+# display_list(restaurants)
+# display_list(entertainments)
+# display_list(transportations)
 
 
-def trip_generator(destinations, restaurants, transportations, entertainments):
-    destination = random.choice(destinations)
-    restaurant = random.choice(restaurants)
-    transportation = random.choice(transportations)
-    entertainment = random.choice(entertainments)
+# def trip_generator(destinations, restaurants, transportations, entertainments):
+#     destination = random.choice(destinations)
+#     restaurant = random.choice(restaurants)
+#     transportation = random.choice(transportations)
+#     entertainment = random.choice(entertainments)
    
+#     print("Here is your potential Day Trip ")
+#     print(f"Destination : {destination}")
+#     print(f"Restaurant: {restaurant}")
+#     print(f"Transportation: {transportation}")
+#     print(f"Entertainment: {entertainment}")
+#     trip_confirmation = input("Do you want to confirm this trip? y/n ")
+#     if trip_confirmation == "y":
+#          print("Trip Confrimed")
+#     else:
+#         trip_generator(destinations, restaurants, transportations, entertainments)
+    
+    
+
+
+
+# trip_generator(destinations, restaurants, transportations, entertainments)
+
+def random_choice(list):
+    return random.choice(list)
+
+
+def print_trip(destination, restaurant, transportation, entertainment):
     print("Here is your potential Day Trip ")
     print(f"Destination : {destination}")
     print(f"Restaurant: {restaurant}")
     print(f"Transportation: {transportation}")
     print(f"Entertainment: {entertainment}")
+
+
+def confrim_trip():
     trip_confirmation = input("Do you want to confirm this trip? y/n ")
     if trip_confirmation == "y":
-         print("Trip Confrimed")
+          print("Trip Confrimed")
     else:
-        trip_generator(destinations, restaurants, transportations, entertainments)
-    
-    
+         trip_generator(destinations, restaurants, transportations, entertainments)
+     
+
+
+
+def trip_generator(destinations, restaurants, transportations, entertainments):
+    destination = random_choice(destinations)
+    restaurant = random_choice(restaurants)
+    transportation = random_choice(transportations)
+    entertainment = random_choice(entertainments)
+    print_trip(destination, restaurant, transportation, entertainment)
+    confrim_trip()
+
 
 
 
 trip_generator(destinations, restaurants, transportations, entertainments)
 
-
-
-
-
-
-def trip_generator(destinations, restaurants, transportations, entertainments):
-    destination = random.choice(destinations)
-    restaurant = random.choice(restaurants)
-    transportation = random.choice(transportations)
-    entertainment = random.choice(entertainments)
-   
-    print("Here is your potential Day Trip ")
-    print(f"Destination : {destination}")
-    print(f"Restaurant: {restaurant}")
-    print(f"Transportation: {transportation}")
-    print(f"Entertainment: {entertainment}")
-    trip_confirmation = input("Do you want to confirm this trip? y/n ")
-    if trip_confirmation == "y":
-         print("Trip Confrimed")
-    elif trip_confirmation == "n":
-        trip_generator(destinations, restaurants, transportations, entertainments)
-    
-    
